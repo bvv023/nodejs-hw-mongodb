548dc090884fc68de66eb34892a595881cd78706
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-09-02 17:40:27 +0300

    google

---
+++ src/utils/googleOAuth2.js
@@ -0,0 +1,53 @@
+// src/utils/googleOAuth2.js
+import { OAuth2Client } from 'google-auth-library';
+import path from 'node:path';
+import { readFile } from 'fs/promises';
+import env from './env.js';
+import createHttpError from 'http-errors';
+
+const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+
+let oauthConfig;
+
+try {
+  const fileContent = await readFile(PATH_JSON);
+  oauthConfig = JSON.parse(fileContent);
+} catch (error) {
+  console.error('Failed to read Google OAuth configuration file:', error);
+  throw createHttpError(500, 'Google OAuth configuration file missing or invalid');
+}
+
+const googleOAuthClient = new OAuth2Client({
+  clientId: env('GOOGLE_AUTH_CLIENT_ID'),
+  clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
+  redirectUri: oauthConfig.web.redirect_uris[0],
+});
+
+export const generateAuthUrl = () =>
+  googleOAuthClient.generateAuthUrl({
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.email',
+      'https://www.googleapis.com/auth/userinfo.profile',
+    ],
+  });
+
+export const validateCode = async (code) => {
+  const response = await googleOAuthClient.getToken(code);
+  if (!response.tokens.id_token) throw createHttpError(401, 'Unauthorized');
+
+  const ticket = await googleOAuthClient.verifyIdToken({
+    idToken: response.tokens.id_token,
+  });
+  return ticket;
+};
+
+export const getFullNameFromGoogleTokenPayload = (payload) => {
+  let fullName = 'Guest';
+  if (payload.given_name && payload.family_name) {
+    fullName = `${payload.given_name} ${payload.family_name}`;
+  } else if (payload.given_name) {
+    fullName = payload.given_name;
+  }
+
+  return fullName;
+};
