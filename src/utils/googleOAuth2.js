4faf03b4ff9210aee6cbeb7d59906f00031e9107
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-09-06 14:56:47 +0300

    останнє

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -1,20 +1,17 @@
 // src/utils/googleOAuth2.js
 import createHttpError from 'http-errors';
-
 import { OAuth2Client } from 'google-auth-library';
 import path from 'node:path';
 import { readFile } from 'fs/promises';
-
 import { env } from './env.js';
 
 const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
-
 const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
   clientId: env('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri: oauthConfig.web.redirect_uris[0], // Оновлений редірект
 });
 
 export const generateAuthUrl = () =>
