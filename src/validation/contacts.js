eecd1c08a9b20792621496b351147553a46fb847
Volodymyr BERESNIEV <bvv023@gmail.com>
2024-09-08 15:23:11 +0300

    qqqqqq

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,20 +2,40 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().optional(),
-  isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string().valid('home', 'personal').required(),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.empty': `"name" is not allowed to be empty`,
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.empty': `"phoneNumber" is not allowed to be empty`,
+  }),
+  email: Joi.string().email().optional().allow(null, '').messages({
+    'string.email': `"email" must be a valid email`,
+    'string.empty': `"email" is not allowed to be empty`,
+  }),
+  isFavourite: Joi.boolean().optional().allow(null),
+  contactType: Joi.string().valid('home', 'personal').required().messages({
+    'any.only': `"contactType" must be one of [home, personal]`,
+    'string.empty': `"contactType" is not allowed to be empty`,
+  }),
   photo: Joi.any().optional(),
 });
 
-
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).optional(),
-  phoneNumber: Joi.string().min(3).max(20).optional(),
-  email: Joi.string().email().optional(),
-  isFavourite: Joi.boolean().optional(),
-  contactType: Joi.string().valid('home', 'personal').optional(),
-  photo: Joi.any().optional(), 
+  name: Joi.string().min(3).max(20).optional().messages({
+    'string.empty': `"name" is not allowed to be empty`,
+  }),
+  phoneNumber: Joi.string().min(3).max(20).optional().messages({
+    'string.empty': `"phoneNumber" is not allowed to be empty`,
+  }),
+  email: Joi.string().email().optional().allow(null, '').messages({
+    'string.email': `"email" must be a valid email`,
+    'string.empty': `"email" is not allowed to be empty`,
+  }),
+  isFavourite: Joi.boolean().optional().allow(null),
+  contactType: Joi.string().valid('home', 'personal').optional().messages({
+    'any.only': `"contactType" must be one of [home, personal]`,
+    'string.empty': `"contactType" is not allowed to be empty`,
+  }),
+  photo: Joi.any().optional(),
 }).min(1);
+
